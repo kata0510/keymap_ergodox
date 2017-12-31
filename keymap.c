a68b90d1d777817692f2f9c3c61b6a1a42344c40
Naoki Katahira <kata0510.com@gmail.com>
2017-12-31 15:08:48 +0900

    記号周りを変更

--- keymap.c
+++ keymap.c
@@ -17,15 +17,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   `    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |    ; |   '    |
- * |--------+------+------+------+------+------| 　　　|           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |    / | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      | LCTRL|  ALT | LGUI |                                       | RGUI |      |   [  |   ]  |   `  |
+ *   |   -  |   =  | LCTRL|  ALT | LGUI |                                       | RGUI |      |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Copy | Paste|       |      | 1Pass|
@@ -37,22 +37,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,    KC_1,   KC_2,     KC_3,     KC_4,   KC_5,   KC_NO,
-        KC_TAB,   KC_Q,   KC_W,     KC_E,     KC_R,   KC_T,   TG(1),
-        KC_LCTRL,  KC_A,   KC_S,     KC_D,     KC_F,   KC_G,
-        KC_LSFT,   KC_Z,   KC_X,     KC_C,     KC_V,   KC_B,   KC_NO,
-        KC_NO,     KC_NO,  KC_LCTRL, KC_LALT,  KC_LGUI,
-
+        KC_ESC,    KC_1,    KC_2,     KC_3,     KC_4,   KC_5,   KC_NO,
+        KC_TAB,    KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,   TG(1),
+        KC_LCTRL,  KC_A,    KC_S,     KC_D,     KC_F,   KC_G,
+        KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,   KC_NO,
+        KC_MINS,   KC_EQL,  KC_LCTRL, KC_LALT,  KC_LGUI,
                                                  M(0),   M(1),
                                                          KC_RIGHT,
                                          KC_SPC, KC_TAB, KC_LEFT,
 
         // right hand 
-        KC_RGHT,  KC_6,  KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,
+        KC_RGHT,  KC_6,  KC_7,    KC_8,     KC_9,    KC_0,     KC_GRV,
         TG(1),    KC_Y,  KC_U,    KC_I,     KC_O,    KC_P,     KC_BSLS,
                   KC_H,  KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
         KC_NO,    KC_N,  KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-                         KC_RGUI, KC_NO,    KC_LBRC, KC_RBRC,  KC_GRV,
+                         KC_RGUI, KC_NO,    KC_LBRC, KC_RBRC,  KC_NO,
 
         KC_LALT, M(2),
         KC_UP,
@@ -69,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |  LShift |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |      | CTRL |  ALT | LGUI |                                       | RGUI |    . |   0  |   =  |      |
+ *   |   -   |   =  | CTRL |  ALT | LGUI |                                       | RGUI |    . |   0  |   =  |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |(TS)CP|(TS)PA|       |　　　 |1Pass |
@@ -88,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
-                                                          RGB_MOD, KC_TRNS,
+                                                KC_TRNS, KC_TRNS,
                                  					               KC_TRNS,
                          					     KC_TRNS, KC_TRNS, KC_TRNS,
 
